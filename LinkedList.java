aba888abc915e79e2eb744e643c06b070937cede
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-10-16 12:26:48 -0500

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -4,24 +4,28 @@ import java.util.*;
 public class LinkedList<T> implements List<T>{
 	private class Node<T>{
 		T data;
-		Node <T> next;
+		ListNode <T> next;
 
-		public Node(T value){
+		public ListNode(T value){
 			data = value;
 			next = null;
 		}
 	}
 	private class ListIterator<T> implements Iterator<T>{
-		private Node node = head;
+		private ListNode node = head;
 		public boolean hasNext(){
 			return node.next != null;
 		}
 		public T next(){
-			Node prev = node;
+			ListNode prev = node;
 			node = node.next;
 			return prev.data;
 		}
 	}
+	//**
+	ListNode<T> head;
+	int size;
+	//**
 	//Constructor : Your list must have a constructor which will make an instance of your linked list. It will take in a variable of type ListNode.
 	public LinkedList(ListNode item){
 		head = null;
@@ -33,35 +37,44 @@ public class LinkedList<T> implements List<T>{
 	//This should not return anything.
 	public void add(ListNode item){
 		if(head == null){
-			head = new Node(item);
+			head = new ListNode(item);
 			++size;
-			return true;
+		}
+		else{
+			ListNode <T> prev = head;
+			while(prev.next!= null){
+				prev=prev.next;
+			}
+			ListNode<T> node = new ListNode(item);
+			prev.next= node;
+			++size;
 		}
 	}
 	public void add(ListNode item, int position){
 		if(position == 0){
-			Node node = new Node(item);
+			ListNode<T> node = new ListNode(item);
 			node.next = head;
 			head = node;
 			++size;
 		}
 		else{
-			Node prev = head;
+			ListNode<T> prev = head;
 			for(int i=0; i<position-1; i++){
 				prev = prev.next;
-				Node node = new Node(item);
-				prev.next = node;
-				++size;
 			}
+			ListNode node = new ListNode(item);
+			prev.next = node;
+			++size;
 		}
 	}
 	//Get : This takes in an integer position which then returns the node at that position in the list. If the position is invalid, return null
 	public ListNode get(int position){
 		//error check
-		///
-		///
+		if(position<size||position>=size){
+			return null;
+		}
 
-		Node curr = head;
+		ListNode<T> curr = head;
 		for (int i = 0; i<position; i++){
 			curr = curr.next;
 		}
@@ -70,28 +83,36 @@ public class LinkedList<T> implements List<T>{
 	//Remove: The function takes in an integer position , which it then both removes and returns the ListNode at that position
 	public ListNode remove(int position){
 		//Error exception
-		//
-		//
 		if (position == 0){
-			Node node = head;
+			ListNode<T> node = head;
 			head = head.next;
 			--size;
 			return node.data;
 		}
 		else{
-			Node prev = head;
+			ListNode<T> prev = head;
 			for (int i=0; i<position-1; i++){
 				prev = prev.next;
 			}
-			Node node = prev.next;
+			ListNode<T> node = prev.next;
 			prev.next = node.next;
 			--size;
 			return node.data;
 		}
 	}
 	//Reverse: The function reverses the whole linked list from head to tail. It should take in a parameter ListNode head. And return back the new head once the function is finished.
-	public ListNode reverse(ListNode head){
-
+	public ListNode<T> reverse(ListNode<T> head){
+		ListNode<T> prev=null;
+		ListNode<T> curr= head;
+		ListNode<T> next= null;
+		while(curr!=null){
+			next= curr.next;
+			curr.next=prev.next;
+			prev=curr;
+			curr=next;
+		}
+		head=prev;
+		return head;
 	}
 	public Iterator iterator(){
 		return new ListIterator();
@@ -109,10 +130,18 @@ public class LinkedList<T> implements List<T>{
 		while (iterator.hasNext()){
 			System.out.println(iterator.next());
 		}
+
+		// ListNode<T> node= head;
+		// while(node!=null){
+		// 	System.out.print(node.data+"==> ");
+		// 	node=node.next;
+		// }
+		// System.out.println();
 	}
 	//Main function
 	public static void main(String[] args){
-		List<Integer> lL = new LinkedList()<Integer>;
+		LinkedList<Integer> lL = new LinkedList()<Integer>;
+
 		//OG list
 		lL = {1, 2, 3, 4, 5}
 
@@ -134,6 +163,7 @@ public class LinkedList<T> implements List<T>{
 
 		//print OG list
 		lL.reverse();
+		lL.head = lL.reverse(lL.head)
 		//print new list
 
 	}
